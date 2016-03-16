7ea9baa516a7016d051f65163102985249adb2bb
Juan Ibiapina <juanibiapina@gmail.com>
2016-03-15 22:39:41 -0300

    feature: Read a line from stdin and write to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_line = String::new();
+
+    stdin().read_line(&mut input_line).unwrap();
+
+    print!("{}", input_line);
 }
